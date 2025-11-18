16c8d91c5d9160a676eb995a764eeb7c39d2b035
whtpdnd <whtpdnd200@gmail.com>
2025-11-18 18:55:25 +0900

    [조세웅] - 예제 진행

--- src/main/java/com/devwork/springexample/thymeleaf/ThymeleafController.java
+++ src/main/java/com/devwork/springexample/thymeleaf/ThymeleafController.java
@@ -1,10 +1,13 @@
 package com.devwork.springexample.thymeleaf;
 
+import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.*;
 
 @RequestMapping("/thymeleaf")
@@ -48,5 +51,20 @@ public class ThymeleafController {
         model.addAttribute("userList", userList);
 
         return "/thymeleaf/ex02";
+    }
+
+    @GetMapping("/ex03")
+    public String ex03(Model model) {
+
+        Date date = new Date();
+
+        LocalDate localDate = LocalDate.now();
+
+        LocalDateTime localDateTime = LocalDateTime.now();
+        model.addAttribute("date", date);
+        model.addAttribute("localDate", localDate);
+        model.addAttribute("localDateTime", localDateTime);
+
+        return "/thymeleaf/ex03";
     }
 }
